d36145b266d9fc53fd5f04b360e35a1c0398b9e0
gl7808 <whorshunter42@yandex.ru>
2022-10-26 15:21:13 +0300

    Переменные

--- Шаблон/js/script.js
+++ Шаблон/js/script.js
@@ -2,4 +2,9 @@
 // Примитивные типы  :  number | string | boolean | null | undefined | symbol 
 // Ссылочные типы    :  object
 
-    
+const objectA = {
+    a: 10,
+    b: true
+}
+
+console.log(objectA.a);
