167c2f9f47a8ae6ddce4a90692f966f0886f17d7
gl7808 <whorshunter42@yandex.ru>
2022-10-26 18:44:37 +0300

    freecodecamp_1

--- Шаблон/js/script.js
+++ Шаблон/js/script.js
@@ -2,9 +2,13 @@
 // Примитивные типы  :  number | string | boolean | null | undefined | symbol 
 // Ссылочные типы    :  object
 
-const objectA = {
-    a: 10,
-    b: true
-}
 
-console.log(objectA.a);
+// Статическая и динамическая типизация
+// JavaScript - языка динамической типиазции
+
+// String a = 'abc';  - пример статической типизации
+    // let a = 'abc';  - как используется в JavaScript
+    //     a = 10; 
+
+    let g = 11 % 3;
+    console.log(g);
